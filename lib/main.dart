909ccf2c0786e177266b4e5b46a2699405a46f73
LarrYdaSnaiL <rafifauzantsany.lubis@gmail.com>
2025-12-18 12:32:54 +0700

    trying to release, again

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,19 @@ import 'package:eventure/navigation/app_router.dart';
 import 'package:eventure/providers/auth_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'firebase_options.dart'; 
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MainApp());
 }
